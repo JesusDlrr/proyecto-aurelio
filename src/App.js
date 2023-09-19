7c5b69700808b7aadc241e5d1800a5a4770a8e2b
roddelapan <j2001.dlrr@gmail.com>
2023-09-18 17:46:57 -0700

    Iniciando con la conexión con firebase

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
+// import { Counter } from './features/counter/Counter';
+// import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "firebase/auth";
 import './App.css';
+import { LoginForm } from './features/login_form/LoginForm';
+
+// function xd()
+// {
+//   const auth = getAuth(app);
+//   onAuthStateChanged(auth, user => {
+//     if(user != null)
+//     {
+//       console.log(user);
+//       return(<p>{user.uid}</p>);
+//     }else{
+//       console.log("Not logged in")
+//       return(<p>Not logged in</p>)
+//     }
+//   });
+
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <LoginForm/>
+    </>
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import useReducer from '../features/login_form/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // counter: counterReducer,
+    user: useReducer
   },
 });
