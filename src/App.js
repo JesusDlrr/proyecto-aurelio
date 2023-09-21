1d35b719a4b8eb807a91e8bdea100dc068343e3f
roddelapan <j2001.dlrr@gmail.com>
2023-09-21 10:52:39 -0700

    Iniciada pantalla home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /*Importes*/
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { LogInForm } from './features/login_form/LoginForm';
 import { SignUpForm } from './features/signup_form/SignUpForm';
+import { Home } from './features/home/Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     {/*Navegacion por las rutas de la pagina*/}
     <Router>
       <Routes>
+        <Route exact path="/" Component={Home}/>
         <Route exact path="/login" Component={LogInForm}/>
         <Route exact path="/signup" Component={SignUpForm}/>
       </Routes>
---
+++ src/features/home/useHome.js
@@ -0,0 +1,11 @@
+const useHome = () => {
+    const post = () => {
+        console.log("post");
+    }
+
+    return {
+        post
+    };
+}
+
+export default useHome;
