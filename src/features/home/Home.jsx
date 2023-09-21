a207ebd1c71f667a0f326a78e5130f9e14eb9d01
roddelapan <j2001.dlrr@gmail.com>
2023-09-21 13:26:47 -0700

    Iniciando la implementacion de los posts en home

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import useHome from "./useHome";
 
 export const Home = () => {
+    const [post_message, setPostMessage] = useState("");
     const {
-        post
+        post,
+        posts
     } = useHome();
 
     return(
         <>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea name="" id="" cols="30" rows="10" onChange={(e) => {setPostMessage(e.target.value)}}></textarea>
             <br />
-            <input type="button" value="POST" onClick={() => {post()}}/>
+            <input type="button" value="POST" onClick={() => {post(post_message)}}/>
+            {posts.map((post) => {
+                return(<>
+                    <div>{post.texto}</div>
+                </>);
+            })}
         </>
     )
 }
--- src/features/home/useHome.js
+++ src/features/home/useHome.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
 const useHome = () => {
-    const post = () => {
-        console.log("post");
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+        const docSnap = await getDocs(collection(db, "publicaciones"));
+
+        setPosts(docSnap.docs.map((doc) => {
+            return doc.data();
+        }));
+    }
+    
+    const post = async (message) => {
+        try {
+            const docRef = await addDoc(collection(db, "publicaciones"), {
+                hora: "Ada",
+                me_gustas: true,
+                texto: message,
+                usuario_id: "69"
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
     }
 
+    getPosts();
+
     return {
-        post
+        post,
+        posts
     };
 }
 
