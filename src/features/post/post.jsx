350b3f3741a429d5516e075cf0c7c6f0187803b1
Jose-Sanchez <josemsanchezq00@gmail.com>
2023-10-30 11:36:02 -0700

    se agrego boton para agregar usuarios dentro del perfil

--- src/features/post/post.jsx
+++ src/features/post/post.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import usePost from "./UsePost";
+import usePost from "./usePost";
 
 export const Post = ({ post }) => {
 
