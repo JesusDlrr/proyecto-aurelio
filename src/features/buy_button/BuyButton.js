2827b6139eed768a7826ce61f5d65b547bbb201e
Jesus de la Rosa <j2001.dlrr@gmail.com>
2024-01-02 14:03:06 -0700

    todo lo de diciembre xd

--- src/features/buy_button/BuyButton.js
+++ src/features/buy_button/BuyButton.js
@@ -7,11 +7,17 @@ function BuyButton() {
 
 
     const a = () => {
-        window.open('/subscriptions/quicker', '_blank', 'noreferrer');
+        window.open('/subscriptions/quicker/monthly', '_blank', 'noreferrer');
+    }
+    const b = () => {
+        window.open('/subscriptions/quicker/yearly', '_blank', 'noreferrer');
     }
     // Paste the stripe-buy-button snippet in your React component
     return (
-        <Button color='black' onClick={a}>Subscribe to Quicker</Button>
+        <>
+            <Button color='black' onClick={a}>Quicker Monthly</Button>
+            <Button color='black' onClick={b}>Quicker Yearly </Button>
+        </>
         // <stripe-buy-button
         //     buy-button-id="buy_btn_1ONNM2KwKqN5WBN1vNRrBBnC"
         //     publishable-key="pk_test_4z52MZdkWrwvVbGFlPdRUIaL002t53Szbt"
